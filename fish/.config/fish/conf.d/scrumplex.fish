ebb4dfa5b2715e0344e7aa9d0059bd1c5f89f137
Sefa Eyeoglu <contact@scrumplex.net>
2021-01-22 11:28:11 +0100

    fish: use conf.d instead of fish_variables

---
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -0,0 +1,12 @@
+# bobthefisher Theme settings
+set -g theme_color_scheme nord
+set -g theme_nerd_fonts yes
+
+
+# Don't truncate $PWD
+set -g fish_prompt_pwd_dir_length 0
+
+
+# Custom $PATH
+set -g fish_user_paths "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin"
+
