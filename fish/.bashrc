1b0e9645ff109f458424cc56bb16a3cc2564e413
Sefa Eyeoglu <contact@scrumplex.net>
2021-02-14 23:01:52 +0100

    fish: add kdesrc-build to path, start fish only, if bash itself wasn't started by fish

--- fish/.bashrc
+++ fish/.bashrc
@@ -8,7 +8,8 @@
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
-if [[ $DISPLAY ]]; then
+# avoid starting fish if the dumb user wants a bash session inside a fish session
+if [ $(ps -p $PPID -o comm=) != "fish" ]; then
     exec fish
 fi
 
--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -8,7 +8,7 @@ set -g fish_prompt_pwd_dir_length 0
 
 
 # Custom $PATH
-set -g fish_user_paths "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin"
+set -g fish_user_paths "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "$HOME/kde/src/kdesrc-build" $fish_user_paths
 
 # GPG Key for package and repo- signing
 set -gx GPGKEY "C10411294912A422"
