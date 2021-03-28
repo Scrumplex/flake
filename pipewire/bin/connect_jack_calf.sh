7728beb90e6298d009ea8d0cecb886c55f916386
Sefa Eyeoglu <contact@scrumplex.net>
2021-03-28 22:08:44 +0200

    pipewire: improve file structure

--- pipewire/bin/connect_jack_calf.sh
+++ pipewire/bin/connect_jack_calf.sh
@@ -5,9 +5,7 @@ calf_client_name="calf-compressor"
 
 sleep 1
 
-
 jack_connect "$voip_sink_monitor:monitor_FL" "$calf_client_name:PW Compressor In #1"
 jack_connect "$voip_sink_monitor:monitor_FR" "$calf_client_name:PW Compressor In #2"
 jack_connect "$calf_client_name:PW Compressor Out #1" "$output_sink_playback:playback_FL"
 jack_connect "$calf_client_name:PW Compressor Out #2" "$output_sink_playback:playback_FR"
-
