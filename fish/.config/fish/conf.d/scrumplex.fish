6d32667a07bc269353e14381cd609091ab63f4ff
Sefa Eyeoglu <contact@scrumplex.net>
2021-04-09 21:22:31 +0200

    fish: i dont like ansible cows

--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -8,8 +8,11 @@ set -g fish_prompt_pwd_dir_length 0
 
 
 # Custom $PATH
-set -g fish_user_paths "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "$HOME/kde/src/kdesrc-build" $fish_user_paths
+set -g fish_user_paths "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "$HOME/kde/src/kdesrc-build" "/usr/lib/ccache/bin" $fish_user_paths
 
 # GPG Key for package and repo- signing
 set -gx GPGKEY "C10411294912A422"
+
+# for real, why is this the default?
+set -gx ANSIBLE_NOCOWS 1
 
