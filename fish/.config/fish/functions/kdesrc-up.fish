863aa5eb95020ff6a8fa4a124d332365d7dca678
Sefa Eyeoglu <contact@scrumplex.net>
2021-04-23 22:35:42 +0200

    fish: improve selection in kdesrc-up

--- fish/.config/fish/functions/kdesrc-up.fish
+++ fish/.config/fish/functions/kdesrc-up.fish
@@ -1,3 +1,3 @@
 function kdesrc-up
-	kdesrc-build --include-dependencies plasma-desktop $argv;
+	kdesrc-build --include-dependencies plasma-workspace plasma-framework plasma-nm plasma-pa plasma-thunderbolt plasma-vault plasma-firewall plasma-workspace-wallpapers kdeplasma-addons krunner milou kwin kscreen sddm-kcm breeze discover print-manager plasma-sdk kaccounts-integration kaccounts-providers kdeconnect-kde plasma-browser-integration xdg-desktop-portal-kde kde-gtk-config khotkeys plasma-desktop systemsettings ksysguard plasma-disks plasma-systemmonitor ksystemstats kinfocenter kmenuedit khelpcenter $argv;
 end
