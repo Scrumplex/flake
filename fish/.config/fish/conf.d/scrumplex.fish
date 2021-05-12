3408d2e6b1bdaab24a4bc56274256a7ec5aaef96
Sefa Eyeoglu <contact@scrumplex.net>
2021-05-12 17:29:11 +0200

    fish: kdesrc-build begone

--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -5,7 +5,7 @@ set -g theme_nerd_fonts "yes"
 set -g fish_prompt_pwd_dir_length 0
 
 # Custom $PATH
-fish_add_path -gp "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "$HOME/kde/src/kdesrc-build" "/usr/lib/ccache/bin"
+fish_add_path -gp "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "/usr/lib/ccache/bin"
 
 # GPG Key for package and repo- signing
 set -gx GPGKEY "C10411294912A422"
