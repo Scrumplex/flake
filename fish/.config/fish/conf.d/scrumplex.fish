c616741b9f48823281c710abfa07abc9b52000b6
Sefa Eyeoglu <contact@scrumplex.net>
2021-05-12 17:28:42 +0200

    fish: use fish_add_path

--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -5,7 +5,7 @@ set -g theme_nerd_fonts "yes"
 set -g fish_prompt_pwd_dir_length 0
 
 # Custom $PATH
-set -g fish_user_paths "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "$HOME/kde/src/kdesrc-build" "/usr/lib/ccache/bin" $fish_user_paths
+fish_add_path -gp "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "$HOME/kde/src/kdesrc-build" "/usr/lib/ccache/bin"
 
 # GPG Key for package and repo- signing
 set -gx GPGKEY "C10411294912A422"
