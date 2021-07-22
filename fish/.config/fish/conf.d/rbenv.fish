4ecbed3be45c6c205344670ee98ea4830b2120ea
Sefa Eyeoglu <contact@scrumplex.net>
2021-07-22 21:15:43 +0200

    fish: load rbenv only if installed

--- fish/.config/fish/conf.d/rbenv.fish
+++ fish/.config/fish/conf.d/rbenv.fish
@@ -1,4 +1,6 @@
 # Load rbenv automatically by appending
 # the following to ~/.config/fish/config.fish:
 
-status --is-interactive; and source (rbenv init -|psub)
+
+
+status --is-interactive; and command -q rbenv; and source (rbenv init -|psub)
