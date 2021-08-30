14615b8de863029233a9ae46a77cbe11cbbb806c
Sefa Eyeoglu <contact@scrumplex.net>
2021-08-30 20:14:39 +0300

    fish: let fish_add_path set user_paths to avoid duplicates

--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -4,7 +4,9 @@ set -g theme_nerd_fonts "yes"
 set -g theme_title_display_process "yes"
 
 # Custom $PATH
-fish_add_path -gp "$HOME/bin" "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" "/usr/lib/ccache/bin"
+fish_add_path -g "/usr/lib/ccache/bin"
+fish_add_path -g "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin"
+fish_add_path -g "$HOME/bin"
 
 # for real, why is this the default?
 set -gx ANSIBLE_NOCOWS 1
