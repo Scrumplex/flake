1996a28d32ff0110ae506d28bef8632ba6799823
Sefa Eyeoglu <contact@scrumplex.net>
2021-09-22 20:39:53 +0200

    base: set $PATH using environment.d

--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -6,7 +6,6 @@ set -g theme_title_display_process "yes"
 # Custom $PATH
 fish_add_path -g "/usr/lib/ccache/bin"
 fish_add_path -g "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin"
-fish_add_path -g "$HOME/bin"
 
 # for real, why is this the default?
 set -gx ANSIBLE_NOCOWS 1
