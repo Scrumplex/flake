382660795e9fd7fbbfe30640d56bbf77eee8a5d2
Sefa Eyeoglu <contact@scrumplex.net>
2021-11-19 17:56:27 +0100

    pipewire: configuration for Modi DAC

--- pipewire/bin/connect_jack_calf.sh
+++ pipewire/bin/connect_jack_calf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 voip_sink_monitor="VoIP Audio/Sink sink Monitor"
-output_sink_playback="Schiit Hel 2 Digital Stereo (IEC958)"
+output_sink_playback="Schiit Modi 3+ Digital Stereo (IEC958)"
 calf_client_name="calf-compressor"
 
 sleep 2
