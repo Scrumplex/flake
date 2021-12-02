bbc2db3440d4ea39a28aa0aa5b735f8c9b090996
Sefa Eyeoglu <contact@scrumplex.net>
2021-12-02 19:38:20 +0100

    sway/fish: only autostart sway in interactive shells

--- sway/.config/fish/conf.d/sway-autostart.fish
+++ sway/.config/fish/conf.d/sway-autostart.fish
@@ -1,3 +1,3 @@
-if test "$XDG_SESSION_TYPE" = "tty"; and test "$XDG_VTNR" -eq 1
+if status --is-interactive; and test -n "$XDG_SESSION_TYPE" -a "$XDG_SESSION_TYPE" = "tty" -a -n "$XDG_VTNR" -a "$XDG_VTNR" -eq 1
         S
 end
