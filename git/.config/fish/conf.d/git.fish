4f295da99c444701dc67555e455dec614929be25
Sefa Eyeoglu <contact@scrumplex.net>
2022-02-08 22:26:00 +0100

    git: add fish abbreviations

---
+++ git/.config/fish/conf.d/git.fish
@@ -0,0 +1,13 @@
+abbr -ag -- g git
+abbr -ag -- ga 'git add'
+abbr -ag -- gap 'git add -p'
+abbr -ag -- gca 'git commit --amend'
+abbr -ag -- gcm 'git commit -m'
+abbr -ag -- gco 'git checkout'
+abbr -ag -- gd 'git diff'
+abbr -ag -- gdc 'git diff --cached'
+abbr -ag -- gl 'git log'
+abbr -ag -- gp 'git push'
+abbr -ag -- gpl 'git pull'
+abbr -ag -- gri 'git rebase --interactive'
+abbr -ag -- gs 'git status'
