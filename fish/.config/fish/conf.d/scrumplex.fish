c3957cb07585efeb3e5e6ef389b7d802c6868122
Sefa Eyeoglu <contact@scrumplex.net>
2022-03-27 16:26:22 +0200

    fish: add perl bin paths

--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -5,6 +5,9 @@ set -g theme_title_display_process "yes"
 
 # Custom $PATH
 fish_add_path -g "/usr/lib/ccache/bin"
+fish_add_path -g "/usr/bin/site_perl"
+fish_add_path -g "/usr/bin/vendor_perl"
+fish_add_path -g "/usr/bin/core_perl"
 
 # for real, why is this the default?
 set -gx ANSIBLE_NOCOWS 1
