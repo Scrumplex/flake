3b7adcff82b67a37747af3aedd3ee3d320513966
Sefa Eyeoglu <contact@scrumplex.net>
2022-05-05 20:48:30 +0200

    fix(traefik): disable unsupported cipher

--- infrastructure/hosts/common/traefik.nix
+++ infrastructure/hosts/common/traefik.nix
@@ -55,7 +55,7 @@
             # TLS 1.3
             "TLS_AES_256_GCM_SHA384"
             "TLS_CHACHA20_POLY1305_SHA256"
-            "TLS_SHA384_SHA384"
+            #"TLS_SHA384_SHA384"  # not support yet
 
             # TLS 1.2
             "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
