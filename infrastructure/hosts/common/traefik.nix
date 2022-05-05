6e7c7218d20e8651ecddf458c037bd5f0c0ba661
Sefa Eyeoglu <contact@scrumplex.net>
2022-05-05 20:46:48 +0200

    refactor(traefik): switch to better ciphers

--- infrastructure/hosts/common/traefik.nix
+++ infrastructure/hosts/common/traefik.nix
@@ -52,10 +52,14 @@
       tls.options.default = {
         minVersion = "VersionTLS12";
         cipherSuites = [
-          "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
-          "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
-          "TLS_AES_256_GCM_SHA384"
-          "TLS_CHACHA20_POLY1305_SHA256"
+            # TLS 1.3
+            "TLS_AES_256_GCM_SHA384"
+            "TLS_CHACHA20_POLY1305_SHA256"
+            "TLS_SHA384_SHA384"
+
+            # TLS 1.2
+            "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+            "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
         ];
         curvePreferences = [
           "CurveP521"
