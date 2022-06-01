083c89db4c830664248b27e5ff735b773942d578
Sefa Eyeoglu <contact@scrumplex.net>
2022-06-01 14:48:20 +0200

    refactor: add all missing keys to secrets

--- infrastructure/secrets/secrets.nix
+++ infrastructure/secrets/secrets.nix
@@ -1,12 +1,16 @@
 let
-  scrumplex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJV9lYhi0kcwAAjPTMl6sycwCGkjrI0bvTIwpPuXkW2W scrumplex@andromeda";
+  scrumplex = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJV9lYhi0kcwAAjPTMl6sycwCGkjrI0bvTIwpPuXkW2W scrumplex@andromeda"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILI4YbjYdiJxS6ptsJ/k161c9SY2yD6siW6Al+xSCDgD scrumplex@doppler"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOXynAF50OShHHCcIGuHO133PWo/pjpaYgyOQDTS5Tj termux@blackeye"
+  ];
 
   spacehub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOPLNX2+TkwkR92aSmNw8faKt4DO58EJkJrBk//MEHrf";
   duckhub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINo54AduiP0MDvRS35SeEV0Wi1Nlszo3enR/xVJtGQaX";
 in
 {
-  "spacehub/id_borgbase.age".publicKeys = [ scrumplex spacehub ];
-  "spacehub/wireguard.key.age".publicKeys = [ scrumplex spacehub ];
-  "duckhub/id_borgbase.age".publicKeys = [ scrumplex duckhub ];
-  "duckhub/wireguard.key.age".publicKeys = [ scrumplex duckhub ];
+  "spacehub/id_borgbase.age".publicKeys = scrumplex ++ [ spacehub ];
+  "spacehub/wireguard.key.age".publicKeys = scrumplex ++ [ spacehub ];
+  "duckhub/id_borgbase.age".publicKeys = scrumplex ++ [ duckhub ];
+  "duckhub/wireguard.key.age".publicKeys = scrumplex ++ [ duckhub ];
 }
