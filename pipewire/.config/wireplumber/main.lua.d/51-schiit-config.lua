a93351cd7ceebe51ae66cb855c7d5d5de5272037
Sefa Eyeoglu <contact@scrumplex.net>
2022-06-04 15:05:53 +0200

    refactor: switch to wireplumber

---
+++ pipewire/.config/wireplumber/main.lua.d/51-schiit-config.lua
@@ -0,0 +1,14 @@
+rule = {
+  matches = {
+    {
+      { "node.name", "equals", "alsa_output.usb-Schiit_Audio_Schiit_Modi_3_-00.iec958-stereo" },
+    },
+  },
+  apply_properties = {
+    ["audio.format"] = "S32_LE",
+    ["audio.rate"] = 96000,
+    ["api.alsa.period-size"] = 128,
+  },
+}
+
+table.insert(alsa_monitor.rules,rule)
