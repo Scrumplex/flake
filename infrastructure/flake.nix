cceb26445c24c3044d8cced69e5f5f60bb7d45be
Sefa Eyeoglu <contact@scrumplex.net>
2022-07-17 10:37:59 +0200

    feat: add devShell

--- infrastructure/flake.nix
+++ infrastructure/flake.nix
@@ -4,41 +4,54 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
         agenix.url = "github:ryantm/agenix";
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, agenix }: {
-        colmena = {
-            meta.name = "scrumplex.net";
-            meta.description = "scrumplex.net Network";
-            meta.nixpkgs = import nixpkgs {
-                system = "x86_64-linux";
+    outputs = { self, nixpkgs, flake-utils, agenix }:
+        {
+            colmena = {
+                meta.name = "scrumplex.net";
+                meta.description = "scrumplex.net Network";
+                meta.nixpkgs = import nixpkgs {
+                    system = "x86_64-linux";
+                };
+
+                defaults = { pkgs, ... }: {
+                    imports = [ "${agenix}/modules/age.nix" ];
+                };
+
+                spacehub = {
+                    deployment.targetHost = "scrumplex.net";
+                    deployment.targetPort = 22701;
+
+                    age.secrets.id_borgbase.file = secrets/spacehub/id_borgbase.age;
+                    age.secrets."wireguard.key".file = secrets/spacehub/wireguard.key.age;
+                    age.secrets."hetzner.key".file = secrets/spacehub/hetzner.key.age;
+
+                    imports = [ ./hosts/spacehub ];
+                };
+
+                duckhub = {
+                    deployment.targetHost = "duckhub.io";
+                    deployment.targetPort = 22701;
+
+                    age.secrets.id_borgbase.file = secrets/duckhub/id_borgbase.age;
+                    age.secrets."wireguard.key".file = secrets/duckhub/wireguard.key.age;
+                    age.secrets."hetzner.key".file = secrets/duckhub/hetzner.key.age;
+
+                    imports = [ ./hosts/duckhub ];
+                };
             };
-
-            defaults = { pkgs, ... }: {
-                imports = [ "${agenix}/modules/age.nix" ];
+        }
+        //
+        flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        rec {
+            devShell = pkgs.mkShell {
+                buildInputs = [
+                    pkgs.colmena
+                    agenix.defaultPackage.${system}
+                ];
             };
-
-            spacehub = {
-                deployment.targetHost = "scrumplex.net";
-                deployment.targetPort = 22701;
-
-                age.secrets.id_borgbase.file = secrets/spacehub/id_borgbase.age;
-                age.secrets."wireguard.key".file = secrets/spacehub/wireguard.key.age;
-                age.secrets."hetzner.key".file = secrets/spacehub/hetzner.key.age;
-
-                imports = [ ./hosts/spacehub ];
-            };
-
-            duckhub = {
-                deployment.targetHost = "duckhub.io";
-                deployment.targetPort = 22701;
-
-                age.secrets.id_borgbase.file = secrets/duckhub/id_borgbase.age;
-                age.secrets."wireguard.key".file = secrets/duckhub/wireguard.key.age;
-                age.secrets."hetzner.key".file = secrets/duckhub/hetzner.key.age;
-
-                imports = [ ./hosts/duckhub ];
-            };
-        };
-    };
+        });
 }
