c5be0b08d163651bb60e1b4c093e9453a60bf4c3
Sefa Eyeoglu <contact@scrumplex.net>
2022-09-26 13:47:49 +0200

    git: signoff all commits

--- git/.config/fish/conf.d/git.fish
+++ git/.config/fish/conf.d/git.fish
@@ -1,10 +1,9 @@
 abbr -ag -- g git
 abbr -ag -- ga 'git add'
 abbr -ag -- gap 'git add -p'
-abbr -ag -- gca 'git commit --amend'
-abbr -ag -- gcm 'git commit -m'
+abbr -ag -- gca 'git commit -s --amend'
+abbr -ag -- gcm 'git commit -sm'
 abbr -ag -- gco 'git checkout'
-abbr -ag -- gcs 'git commit -sm'
 abbr -ag -- gd 'git diff'
 abbr -ag -- gdc 'git diff --cached'
 abbr -ag -- gl 'git log'
