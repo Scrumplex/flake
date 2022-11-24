8be6a2fdcb86380ab3e4226f86c478198e2db4a1
Sefa Eyeoglu <contact@scrumplex.net>
2022-11-24 09:13:23 +0100

    feat: bump to nixos-22.11
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- infrastructure/flake.nix
+++ infrastructure/flake.nix
@@ -2,7 +2,7 @@
   description = "scrumplex.net Infrastructure";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     agenix.url = "github:ryantm/agenix";
     flake-utils.url = "github:numtide/flake-utils";
   };
