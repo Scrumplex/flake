c215004564710cb6be60433214c40668567c8d9f
Sefa Eyeoglu <contact@scrumplex.net>
2022-12-24 12:56:52 +0100

    fish: add ROCm to PATH
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- fish/.config/fish/conf.d/scrumplex.fish
+++ fish/.config/fish/conf.d/scrumplex.fish
@@ -9,6 +9,7 @@ fish_add_path -g "/usr/lib/ccache/bin"
 fish_add_path -g "/usr/bin/site_perl"
 fish_add_path -g "/usr/bin/vendor_perl"
 fish_add_path -g "/usr/bin/core_perl"
+fish_add_path -g "/opt/rocm/bin"
 
 # for real, why is this the default?
 set -gx ANSIBLE_NOCOWS 1
