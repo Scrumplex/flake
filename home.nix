7c405256c4a367efc52f108300ef88f019e3021b
Sefa Eyeoglu <contact@scrumplex.net>
2023-01-30 13:34:09 +0100

    fontconfig: add Nerd Font Symbols overlay
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- home.nix
+++ home.nix
@@ -29,6 +29,39 @@
     };
   };
   fonts.fontconfig.enable = true;
+  xdg.configFile."fontconfig/conf.d/10-nerd-font-symbols.conf" = {
+    text = ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+      <fontconfig>
+        <alias>
+          <family>Fira Code</family>
+          <prefer><family>Symbols Nerd Font</family></prefer>
+        </alias>
+        <alias>
+          <family>Fira Code,Fira Code Medium</family>
+          <prefer><family>Symbols Nerd Font</family></prefer>
+        </alias>
+        <alias>
+          <family>Fira Code,Fira Code SemiBold</family>
+          <prefer><family>Symbols Nerd Font</family></prefer>
+        </alias>
+        <alias>
+          <family>Fira Code,Fira Code Light</family>
+          <prefer><family>Symbols Nerd Font</family></prefer>
+        </alias>
+        <alias>
+          <family>Fira Code,Fira Code Retina</family>
+          <prefer><family>Symbols Nerd Font</family></prefer>
+        </alias>
+        <alias>
+          <family>Monocraft</family>
+          <prefer><family>Symbols Nerd Font</family></prefer>
+        </alias>
+      </fontconfig>
+    '';
+    onChange = "${pkgs.fontconfig}/bin/fc-cache -f";
+  };
 
   home.packages = [
     pkgs.git-crypt
