afac6afe06798d3cf43c11eca151f8cf95bd03c8
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-01 18:31:36 +0100

    sway/wob: move to Nix
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- home/sway.nix
+++ home/sway.nix
@@ -584,6 +584,37 @@
     temperature.night = 3000;
   };
   services.xembed-sni-proxy.enable = true;
+  systemd.user.services.wob = {
+    Unit = {
+      Description = "A lightweight overlay volume/backlight/progress/anything bar for Wayland";
+      Documentation = "man:wob(1)";
+      PartOf = "graphical-session.target";
+      After = "graphical-session.target";
+      ConditionEnvironment = "WAYLAND_DISPLAY";
+    };
+    Service = {
+      StandardInput = "socket";
+      ExecStart = "${pkgs.wob}/bin/wob";
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
+  systemd.user.sockets.wob = {
+    Socket = {
+      ListenFIFO = "%t/wob.sock";
+      SocketMode = "0600";
+      RemoveOnStop = "yes";
+      FlushPending = "yes";
+    };
+    Install.WantedBy = [ "sockets.target" ];
+  };
+  xdg.configFile."wob/wob.ini".text = ''
+    border_offset=0 
+    border_size=2
+    bar_padding=8
+    border_color=89dcebff 
+    background_color=11111be6 
+    bar_color=89dcebff
+  '';
 
   home.packages = with pkgs; [
     pkgs.wl-clipboard
