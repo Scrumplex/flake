8cfbe15e77c6f4023f6407573d9285b3f6a82d86
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-04 20:32:31 +0100

    andromeda: enable ccache
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- hosts/andromeda/default.nix
+++ hosts/andromeda/default.nix
@@ -87,6 +87,11 @@
     storeOnly = true;
   };
 
+  programs.ccache = {
+    enable = true;
+    packageNames = [ "prismlauncher" ];
+  };
+
   programs.steam.enable = true;
   programs.fish.enable = true;
 
