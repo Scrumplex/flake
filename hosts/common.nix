e42eb06d7542062ec530ca526a32fc7fe5a1054b
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-05 23:41:39 +0100

    common: allow systemctl without password
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  security.sudo.extraRules = [{
+    groups = [ "wheel" ];
+    commands = [{
+      command = "${pkgs.systemd}/bin/systemctl";
+      options = [ "NOPASSWD" ];
+    }];
+  }];
+
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.trusted-users = [ "root" ];
