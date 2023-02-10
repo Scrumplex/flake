25abfaac477730369ff29e787b20470887b163e8
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-10 22:42:48 +0100

    refactor: move run-or-raise into package
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- homes/scrumplex/sway.nix
+++ homes/scrumplex/sway.nix
@@ -1,36 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  run-or-raise = pkgs.writeShellScript "run-or-raise.sh" ''
-    # Copyright (C) 2020-2021 Bob Hepple <bob.hepple@gmail.com>
-    # Copyright (C) 2021 Sefa Eyeoglu <contact@scrumplex.net>
-
-    # This program is free software: you can redistribute it and/or modify
-    # it under the terms of the GNU General Public License as published by
-    # the Free Software Foundation, either version 3 of the License, or (at
-    # your option) any later version.
-    # 
-    # This program is distributed in the hope that it will be useful, but
-    # WITHOUT ANY WARRANTY; without even the implied warranty of
-    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-    # General Public License for more details.
-    # 
-    # You should have received a copy of the GNU General Public License
-    # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-    class="$1"
-    runstring="$2"
-
-    [[ -z "$class" ]] && exit 1
-    [[ -z "$runstring" ]] && exit 1
-
-    ${pkgs.sway}/bin/swaymsg "[app_id=$class] focus" &>/dev/null || {
-      # could be Xwayland app:
-      ${pkgs.sway}/bin/swaymsg "[class=$class] focus" &>/dev/null
-    } || exec $runstring
-
-    exit 0
-  '';
+  run-or-raise = "${pkgs.run-or-raise}/bin/run-or-raise";
   termapp = pkgs.writeShellScript "termapp.sh" ''
     if [ $# -eq 0 ]; then
       exit 2
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,8 @@
 { lib, config, pkgs, ... }:
 
 {
+  run-or-raise = pkgs.callPackage ./tools/wayland/run-or-raise { };
+
   zoom65-udev-rules =
     pkgs.callPackage ./os-specific/linux/zoom65-udev-rules { };
 }
