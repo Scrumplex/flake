bcd2b5483e3cdb790694f47914b4943cf8485b7b
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-15 22:51:27 +0100

    dyson: configure wireguard
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- hosts/dyson/default.nix
+++ hosts/dyson/default.nix
@@ -1,38 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ./boot.nix ];
+  imports = [ ./hardware-configuration.nix ./boot.nix ./wireguard.nix ];
 
   hardware.enableRedistributableFirmware = true;
 
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 
   time.timeZone = "Europe/Berlin";
-
-  networking.wireguard.interfaces.wg-scrumplex = {
-    ips = [ "10.255.255.10/24" "fd69:5efa:5efa:5efa::10/64" ];
-    peers = [
-      {
-        allowedIPs = [ "10.255.255.0/24" "fd69:5efa:5efa:5efa::/64" ];
-        endpoint = "scrumplex.net:22701";
-        publicKey = "1FEGWV0GPVjc4NUprtuwg/bO0jUsUJbE74T6J4tgdVM=";
-        persistentKeepalive = 60;
-      }
-      {
-        allowedIPs = [ "10.255.255.2" "fd69:5efa:5efa:5efa::2" ];
-        endpoint = "duckhub.io:22701";
-        publicKey = "Ny2jkcZzKjKpEnZeUGDv98B2trGqARLsKlhFGkZX7x4=";
-        persistentKeepalive = 60;
-      }
-      {
-        allowedIPs = [ "10.255.255.11" "fd69:5efa:5efa:5efa::11" ];
-        endpoint = "10.10.10.11:22701";
-        publicKey = "SpTbip3iTqRM4qPFy4PtXI2WE9VmRVnfV931LPl3IWs=";
-        persistentKeepalive = 60;
-      }
-    ];
-    privateKeyFile = "/etc/nixos/wg-scrumplex.key";
-  };
 
   networking.networkmanager.enable = true;
 
