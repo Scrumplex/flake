25e2242e93070b5e3a69f84805f872d85cbf9e18
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-17 11:18:57 +0100

    scrumplex: configure k9s
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- homes/scrumplex/common/default.nix
+++ homes/scrumplex/common/default.nix
@@ -99,4 +99,15 @@
   xsession.preferStatusNotifierItems = true; # needed for network-manager-applet
   services.network-manager-applet.enable =
     lib.mkDefault nixosConfig.networking.networkmanager.enable;
+
+  programs.k9s.enable = true;
+  xdg.configFile."k9s/skin.yml".source = let
+    theme = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "k9s";
+      rev = "322598e19a4270298b08dc2765f74795e23a1615";
+      sha256 = "GrRCOwCgM8BFhY8TzO3/WDTUnGtqkhvlDWE//ox2GxI=";
+    };
+  in "${theme}/dist/mocha.yml";
+
 }
