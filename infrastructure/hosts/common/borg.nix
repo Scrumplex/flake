1ebc290cbffc4d4aeb2abcf78d626ba7a2ee0f39
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-25 00:12:36 +0100

    chore: deadnix
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- infrastructure/hosts/common/borg.nix
+++ infrastructure/hosts/common/borg.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, ... }:
 
 {
   services.borgbackup.jobs.borgbase = {
--- infrastructure/hosts/common/traefik.nix
+++ infrastructure/hosts/common/traefik.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, ... }:
 
 {
   networking.firewall.allowedTCPPorts = [ 80 443 ];
