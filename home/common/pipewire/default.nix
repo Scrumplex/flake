1feea256540846fc898950ed1e07664230343fbb
Sefa Eyeoglu <contact@scrumplex.net>
2023-03-23 15:17:24 +0100

    home/pipewire: add wireplumber config for Schiit Modi 3+
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- home/common/pipewire/default.nix
+++ home/common/pipewire/default.nix
@@ -17,4 +17,20 @@
       #};
     };
   };
+
+  xdg.configFile."wireplumber/main.lua.d/51-schiit.lua".text = ''
+    rule = {
+      matches = {
+        {
+          { "node.name", "equals", "alsa_output.usb-Schiit_Audio_Schiit_Modi_3_-00.analog-stereo" },
+        },
+      },
+      apply_properties = {
+        ["audio.format"] = "S32_LE",
+        ["audio.rate"] = 96000,
+        ["api.alsa.period-size"] = 128,
+      },
+    }
+    table.insert(alsa_monitor.rules,rule)
+  '';
 }
