da253ea397bb76dc2df73357842b871a4f9004ea
Sefa Eyeoglu <contact@scrumplex.net>
2023-04-02 23:13:52 +0200

    hosts/common: add termux ssh key
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- infrastructure/hosts/common/common.nix
+++ infrastructure/hosts/common/common.nix
@@ -23,6 +23,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJV9lYhi0kcwAAjPTMl6sycwCGkjrI0bvTIwpPuXkW2W scrumplex@andromeda"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHsd6Edr19iTS5QFnCEvMQh0rUZM1mjksaZHlihweLdU scrumplex@dyson"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOXynAF50OShHHCcIGuHO133PWo/pjpaYgyOQDTS5Tj termux@void"
       ];
     };
   };
