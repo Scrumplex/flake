92082290437a71d9e30da80d9940a8da5c190125
Sefa Eyeoglu <contact@scrumplex.net>
2023-05-13 18:25:12 +0200

    modules/hm/fuzzel: remove
    Upstreamed! See https://github.com/nix-community/home-manager/pull/3726
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- flake.nix
+++ flake.nix
@@ -141,7 +141,6 @@
       hmModules = {
         catppuccin = import ./modules/hm/catppuccin.nix;
         fish-theme = import ./modules/hm/fish-theme.nix;
-        fuzzel = import ./modules/hm/fuzzel.nix;
         jellyfin-mpv-shim = import ./modules/hm/jellyfin-mpv-shim.nix;
         pipewire = import ./modules/hm/pipewire.nix;
       };
