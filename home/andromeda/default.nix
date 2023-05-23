8ebdd09355b72a25389c5f983a2f8035cce99b80
Sefa Eyeoglu <contact@scrumplex.net>
2023-05-23 11:02:59 +0200

    home/sway: disable camera-blank on dyson
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- home/andromeda/default.nix
+++ home/andromeda/default.nix
@@ -1,3 +1,5 @@
 {...}: {
   imports = [./borg.nix ./dev.nix];
+
+  s.waybar.cameraBlank.cameraPath = "/dev/v4l/by-id/usb-046d_Logitech_Webcam_C925e_D8A39E5F-video-index0";
 }
--- home/common/sway/default.nix
+++ home/common/sway/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
@@ -231,7 +232,11 @@
 
         "${mod}+XF86AudioMute" = "exec ${config.programs.waybar.settings.mainBar."custom/pa-mute".on-click}";
         "${mod}+m" = "exec ${config.programs.waybar.settings.mainBar."custom/pa-mute".on-click}";
-        "${mod}+n" = "exec ${config.programs.waybar.settings.mainBar."custom/camera-blank".on-click}";
+        # TODO: make this nice
+        "${mod}+n" =
+          if config.s.waybar.cameraBlank.enable
+          then "exec ${config.programs.waybar.settings.mainBar."custom/camera-blank".on-click}"
+          else "exec echo noop";
 
         "Shift+XF86AudioRaiseVolume" = "exec ${mpc} vol +2 && ${mpc} vol | ${sed} 's|n/a|0%|g;s/[^0-9]*//g' > $XDG_RUNTIME_DIR/wob.sock";
         "Shift+XF86AudioLowerVolume" = "exec ${mpc} vol -2 && ${mpc} vol | ${sed} 's|n/a|0%|g;s/[^0-9]*//g' > $XDG_RUNTIME_DIR/wob.sock";
