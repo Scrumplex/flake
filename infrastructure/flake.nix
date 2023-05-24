f20af686f4304e092ee2390e9e3b5fa43db9493e
Sefa Eyeoglu <contact@scrumplex.net>
2023-05-24 09:13:00 +0200

    flake: update to 23.05
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- infrastructure/flake.nix
+++ infrastructure/flake.nix
@@ -2,7 +2,7 @@
   description = "scrumplex.net Infrastructure";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     agenix = {
