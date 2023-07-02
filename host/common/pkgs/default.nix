e00a2d5f6c0823ba054d13f4624296bcd918a5c8
Sefa Eyeoglu <contact@scrumplex.net>
2023-07-02 10:32:31 +0200

    hosts/pkgs: remove beets override
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- host/common/pkgs/default.nix
+++ host/common/pkgs/default.nix
@@ -10,15 +10,6 @@ in {
 
     overlays = [
       (final: prev: {
-        # https://github.com/NixOS/nixpkgs/pull/240720
-        beets = prev.beets.overrideAttrs (previousAttrs: {
-          patches =
-            previousAttrs.patches
-            ++ [
-              ./beets-remove-failing-embedart-test.patch
-            ];
-        });
-
         discord = mkDiscordOverride prev.discord;
         discord-canary = mkDiscordOverride prev.discord-canary;
         discord-ptb = mkDiscordOverride prev.discord-ptb;
