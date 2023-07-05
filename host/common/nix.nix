8beb1f731e05ed8b196d2f8160addbc9e6079d83
Sefa Eyeoglu <contact@scrumplex.net>
2023-07-05 13:19:29 +0200

    host/nix: add nix-community substitutor
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- host/common/nix.nix
+++ host/common/nix.nix
@@ -5,6 +5,12 @@ in {
     settings = {
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes" "repl-flake" "no-url-literals"];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       trusted-users = ["root"];
     };
     gc = {
