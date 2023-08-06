426756f7de330ad3de295d3fc3f692bbcde9dc3e
Sefa Eyeoglu <contact@scrumplex.net>
2023-08-06 09:58:18 +0200

    host/misc: enable minecraft ports in firewall
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- host/common/misc.nix
+++ host/common/misc.nix
@@ -11,11 +11,13 @@
   networking.firewall = {
     allowedTCPPorts = [
       22000 # syncthing
+      25565 # minecraft
     ];
     allowedUDPPorts = [
       21027 # syncthing
       22000 # syncthing
       24727 # AusweisApp2
+      25565 # minecraft
     ];
   };
 
