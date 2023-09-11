15bc84359eab910df6f448a5cf7f663423528ea1
Sefa Eyeoglu <contact@scrumplex.net>
2023-09-11 16:58:35 +0200

    host/pkgs: remove jellyfin-mpv-shim override
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- host/common/pkgs/default.nix
+++ host/common/pkgs/default.nix
@@ -27,18 +27,6 @@ in {
 
         element-desktop = prev.element-desktop.override {electron = final.electron_24;};
 
-        # https://github.com/NixOS/nixpkgs/pull/247691
-        jellyfin-mpv-shim = prev.jellyfin-mpv-shim.overrideAttrs (prev: {
-          postPatch =
-            prev.postPatch
-            + ''
-              # python-mpv renamed to mpv with 1.0.4
-              substituteInPlace setup.py \
-                --replace "python-mpv" "mpv" \
-                --replace "mpv-jsonipc" "python_mpv_jsonipc"
-            '';
-        });
-
         ncmpcpp = prev.ncmpcpp.override {
           visualizerSupport = true;
         };
