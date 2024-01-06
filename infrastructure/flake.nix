dc167d802159651891faad7a346ccf8142bfa45b
Sefa Eyeoglu <contact@scrumplex.net>
2024-01-06 23:41:32 +0100

    chore: improve lock
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- infrastructure/flake.nix
+++ infrastructure/flake.nix
@@ -24,6 +24,7 @@
     arion = {
       url = "github:hercules-ci/arion";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
     };
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
