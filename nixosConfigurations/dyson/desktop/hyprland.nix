eff6f9647e36b976fb46a93c876d9fd58eaa8331
Sefa Eyeoglu <contact@scrumplex.net>
2024-02-19 19:38:53 +0100

    nixosConfigurations/desktop/hyprland: migrate device configs to hyprlang
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/dyson/desktop/hyprland.nix
+++ nixosConfigurations/dyson/desktop/hyprland.nix
@@ -1,7 +1,9 @@
 {...}: {
   hm.wayland.windowManager.hyprland.settings = {
-    "device:hp,-inc-hyperx-pulsefire-haste-2".sensitivity = -1.0;
-    "device:pixa3854:00-093a:0274-touchpad".natural_scroll = true;
+    "device" = {
+      name = "pixa3854:00-093a:0274-touchpad";
+      natural_scroll = true;
+    };
     "monitor" = [
       "desc:BOE 0x095F,2256x1504@59.999Hz,0x0,1.175"
     ];
