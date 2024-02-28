41c63b31d23348c2a6f05451e07cf62718259149
Sefa Eyeoglu <contact@scrumplex.net>
2024-02-28 10:18:18 +0100

    nixosConfigurations/desktop/pipewire: move schiit rules to derivation
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/desktop/pipewire/default.nix
+++ nixosConfigurations/common/desktop/pipewire/default.nix
@@ -8,6 +8,28 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+
+    wireplumber.configPackages = [
+      (pkgs.writeTextFile {
+        name = "schiit-wireplumber-rules";
+        text = ''
+          rule = {
+            matches = {
+              {
+                { "node.name", "equals", "alsa_output.usb-Schiit_Audio_Schiit_Modi_3_-00.analog-stereo" },
+              },
+            },
+            apply_properties = {
+              ["audio.format"] = "S32_LE",
+              ["audio.rate"] = 96000,
+              ["api.alsa.period-size"] = 128,
+            },
+          }
+          table.insert(alsa_monitor.rules,rule)
+        '';
+        destination = "/share/wireplumber/main.lua.d/51-schiit.lua";
+      })
+    ];
   };
 
   hm.services.pipewire = {
@@ -24,20 +46,4 @@
       #};
     };
   };
-
-  environment.etc."wireplumber/main.lua.d/51-schiit.lua".text = ''
-    rule = {
-      matches = {
-        {
-          { "node.name", "equals", "alsa_output.usb-Schiit_Audio_Schiit_Modi_3_-00.analog-stereo" },
-        },
-      },
-      apply_properties = {
-        ["audio.format"] = "S32_LE",
-        ["audio.rate"] = 96000,
-        ["api.alsa.period-size"] = 128,
-      },
-    }
-    table.insert(alsa_monitor.rules,rule)
-  '';
 }
