57ce296f03ed1cd91a58522a868e41605d61468b
Sefa Eyeoglu <contact@scrumplex.net>
2024-03-01 15:58:29 +0100

    nixosConfigurations: refactor imports
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/default.nix
+++ nixosConfigurations/common/default.nix
@@ -4,59 +4,7 @@
   ...
 }: {
   imports =
-    [
-      ./beets.nix
-      ./bluetooth.nix
-      ./boot.nix
-      ./desktop
-      ./desktop/chromium.nix
-      ./desktop/evolution.nix
-      ./desktop/firefox.nix
-      ./desktop/fuzzel.nix
-      ./desktop/gaming.nix
-      ./desktop/gtk
-      ./desktop/hyprland.nix
-      ./desktop/image-viewer.nix
-      ./desktop/kitty.nix
-      ./desktop/mako.nix
-      ./desktop/nextcloud-client.nix
-      ./desktop/obs.nix
-      ./desktop/pipewire
-      ./desktop/polkit-agent.nix
-      ./desktop/qt
-      ./desktop/screenshot-bash.nix
-      ./desktop/session-lock.nix
-      ./desktop/swayidle.nix
-      ./desktop/vr
-      ./desktop/waybar.nix
-      ./desktop/wlogout.nix
-      ./desktop/wlsunset.nix
-      ./desktop/wob.nix
-      ./desktop/xwaylandvideobridge.nix
-      ./fish.nix
-      ./git.nix
-      ./gpg.nix
-      ./home.nix
-      ./htop.nix
-      ./misc.nix
-      ./mpd.nix
-      ./mpv.nix
-      ./neovim.nix
-      ./nix.nix
-      ./nix-index.nix
-      ./nvd.nix
-      ./password-store.nix
-      ./pkgs
-      ./ranger.nix
-      ./regional.nix
-      ./rtl-sdr.nix
-      ./ssh.nix
-      ./syncthing.nix
-      ./utils.nix
-      ./v4l2loopback.nix
-
-      inputs.agenix.nixosModules.age
-    ]
+    [inputs.agenix.nixosModules.age]
     ++ builtins.attrValues inputs.scrumpkgs.nixosModules;
 
   nixpkgs.overlays = lib.mkBefore [
--- nixosConfigurations/dyson/default.nix
+++ nixosConfigurations/dyson/default.nix
@@ -5,6 +5,54 @@
 
     modules = [
       ../common
+      ../common/beets.nix
+      ../common/bluetooth.nix
+      ../common/boot.nix
+      ../common/desktop
+      ../common/desktop/chromium.nix
+      ../common/desktop/evolution.nix
+      ../common/desktop/firefox.nix
+      ../common/desktop/fuzzel.nix
+      ../common/desktop/gaming.nix
+      ../common/desktop/gtk
+      ../common/desktop/hyprland.nix
+      ../common/desktop/image-viewer.nix
+      ../common/desktop/kitty.nix
+      ../common/desktop/mako.nix
+      ../common/desktop/nextcloud-client.nix
+      ../common/desktop/obs.nix
+      ../common/desktop/pipewire
+      ../common/desktop/polkit-agent.nix
+      ../common/desktop/qt
+      ../common/desktop/screenshot-bash.nix
+      ../common/desktop/session-lock.nix
+      ../common/desktop/swayidle.nix
+      ../common/desktop/waybar.nix
+      ../common/desktop/wlogout.nix
+      ../common/desktop/wlsunset.nix
+      ../common/desktop/wob.nix
+      ../common/desktop/xwaylandvideobridge.nix
+      ../common/fish.nix
+      ../common/git.nix
+      ../common/gpg.nix
+      ../common/home.nix
+      ../common/htop.nix
+      ../common/misc.nix
+      ../common/mpd.nix
+      ../common/mpv.nix
+      ../common/neovim.nix
+      ../common/nix.nix
+      ../common/nix-index.nix
+      ../common/nvd.nix
+      ../common/password-store.nix
+      ../common/pkgs
+      ../common/ranger.nix
+      ../common/regional.nix
+      ../common/rtl-sdr.nix
+      ../common/ssh.nix
+      ../common/syncthing.nix
+      ../common/utils.nix
+      ../common/v4l2loopback.nix
       ../../home
 
       ./configuration.nix
