b06f684a5455ebe365ddd93170623e673f4b1f3c
Sefa Eyeoglu <contact@scrumplex.net>
2024-03-02 11:04:59 +0100

    nixosConfigurations/dyson/hyprland: enable gestures
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/dyson/desktop/hyprland.nix
+++ nixosConfigurations/dyson/desktop/hyprland.nix
@@ -1,10 +1,15 @@
 {...}: {
   hm.wayland.windowManager.hyprland.settings = {
-    "device" = {
+    device = {
       name = "pixa3854:00-093a:0274-touchpad";
       natural_scroll = true;
     };
-    "monitor" = [
+    gestures = {
+      workspace_swipe = true;
+      workspace_swipe_distance = 500;
+      workspace_swipe_forever = true;
+    };
+    monitor = [
       "desc:BOE 0x095F,2256x1504@59.999Hz,0x0,1.175"
     ];
   };
