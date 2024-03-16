9fdc3a2771dd6c6a8972959e6482483f915d961f
Sefa Eyeoglu <contact@scrumplex.net>
2024-03-16 12:11:23 +0100

    nixosConfigurations/common/neovim: fix mapping
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/neovim.nix
+++ nixosConfigurations/common/neovim.nix
@@ -98,19 +98,15 @@
               groupIndex = 2;
             }
           ];
-          mappingPresets = ["insert" "cmdline"];
-          mapping = {
-            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-e>" = ''
-              cmp.mapping({
-                i = cmp.mapping.abort(),
-                c = cmp.mapping.close(),
-              })
-            '';
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
-          };
+          mapping.__raw = ''
+            cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            })
+          '';
           snippet.expand = ''
             function(args)
               require('luasnip').lsp_expand(args.body)
