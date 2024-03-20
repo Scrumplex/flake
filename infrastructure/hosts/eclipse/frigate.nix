1180689f770fc8e029bba294593f3667e8a351b4
Sefa Eyeoglu <contact@scrumplex.net>
2024-03-20 23:14:46 +0100

    hosts/eclipse/frigate: add remaining configuration
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- infrastructure/hosts/eclipse/frigate.nix
+++ infrastructure/hosts/eclipse/frigate.nix
@@ -15,23 +15,59 @@
           roles = ["detect"];
         }
       ];
+      back.ffmpeg.inputs = [
+        {
+          path = "rtsp://127.0.0.1:8554/back";
+          roles = ["record"];
+        }
+        {
+          path = "rtsp://127.0.0.1:8554/back_sub";
+          roles = ["detect"];
+        }
+      ];
+      door.ffmpeg.inputs = [
+        {
+          path = "rtsp://127.0.0.1:8554/door";
+          roles = ["record"];
+        }
+        {
+          path = "rtsp://127.0.0.1:8554/door_sub";
+          roles = ["detect"];
+        }
+      ];
     };
     detect = {
       width = 1280;
       height = 720;
+    };
+    detectors.ov = {
+      type = "openvino";
+      device = "AUTO";
+      model.path = "/openvino-model/ssdlite_mobilenet_v2.xml";
     };
     environment_vars.LIBVA_DRIVER_NAME = "radeonsi";
     ffmpeg.hwaccel_args = "preset-vaapi";
     go2rtc.streams = {
       front = "rtsp://stream:{FRIGATE_RTSP_PASSWORD}@10.10.10.210:554/Streaming/Channels/101";
       front_sub = "rtsp://stream:{FRIGATE_RTSP_PASSWORD}@10.10.10.210:554/Streaming/Channels/102";
+      back = "rtsp://stream:{FRIGATE_RTSP_PASSWORD}@10.10.10.211:554/Streaming/Channels/101";
+      back_sub = "rtsp://stream:{FRIGATE_RTSP_PASSWORD}@10.10.10.211:554/Streaming/Channels/102";
+      door = "rtsp://stream:{FRIGATE_RTSP_PASSWORD}@10.10.10.212:554/Streaming/Channels/101";
+      door_sub = "rtsp://stream:{FRIGATE_RTSP_PASSWORD}@10.10.10.212:554/Streaming/Channels/102";
+    };
+    model = {
+      width = 300;
+      height = 300;
+      input_tensor = "nchw";
+      input_pixel_format = "bgr";
+      labelmap_path = "/openvino-model/coco_91cl_bkgr.txt";
     };
     mqtt.enabled = false;
     record = {
       enabled = true;
       retain = {
-        days = 14;
-        mode = "motion";
+        days = 7;
+        mode = "all";
       };
       events.retain = {
         default = 30;
