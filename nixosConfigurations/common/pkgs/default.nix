c06fc7fec9b1108a7e40950300738aaa947e8953
Sefa Eyeoglu <contact@scrumplex.net>
2024-04-25 13:46:37 +0200

    nixosConfigurations/common/pkgs: remove waybar workaround
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/pkgs/default.nix
+++ nixosConfigurations/common/pkgs/default.nix
@@ -34,20 +34,6 @@ in {
         prismlauncher = prev.prismlauncher.override {
           glfw = final.glfwUnstable;
         };
-
-        # https://github.com/NixOS/nixpkgs/pull/297158
-        waybar = prev.waybar.override {
-          wireplumber = final.wireplumber.overrideAttrs (attrs: rec {
-            version = "0.4.17";
-            src = final.fetchFromGitLab {
-              domain = "gitlab.freedesktop.org";
-              owner = "pipewire";
-              repo = "wireplumber";
-              rev = version;
-              hash = "sha256-vhpQT67+849WV1SFthQdUeFnYe/okudTQJoL3y+wXwI=";
-            };
-          });
-        };
       })
     ];
   };
