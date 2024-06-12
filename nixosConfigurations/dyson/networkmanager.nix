d82228c4d2ff59e9f9b3b9b7343cd6c41cdad154
Sefa Eyeoglu <contact@scrumplex.net>
2024-06-12 16:32:04 +0200

    nixosConfigurations/dyson/networkmanager: fix WireGuard VPN
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/dyson/networkmanager.nix
+++ nixosConfigurations/dyson/networkmanager.nix
@@ -1,8 +1,20 @@
-{...}: {
+{config, ...}: {
+  assertions = [
+    {
+      assertion = !config.networking.nftables.enable;
+      message = "Wireguard isn't configured with networking.nftables.enable enabled";
+    }
+  ];
+
   networking.networkmanager.enable = true;
 
   hm = {
     xsession.preferStatusNotifierItems = true; # needed for network-manager-applet
     services.network-manager-applet.enable = true;
+  };
+
+  networking.firewall = {
+    trustedInterfaces = ["wg-home"];
+    checkReversePath = false;
   };
 }
