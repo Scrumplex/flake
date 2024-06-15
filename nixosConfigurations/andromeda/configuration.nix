0bebe911d649282bc1b08b1b308cdc3c7fd1dc76
Sefa Eyeoglu <contact@scrumplex.net>
2024-06-15 10:51:26 +0200

    nixosConfigurations/andromeda: remove amdvlk config
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/andromeda/configuration.nix
+++ nixosConfigurations/andromeda/configuration.nix
@@ -19,7 +19,6 @@
   hardware.enableRedistributableFirmware = true;
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 
-  hardware.amdgpu.amdvlk = false;
   profile.vr.enableHighPrioKernelPatch = true;
 
   hardware.opengl.vdpau.driverName = "radeonsi";
