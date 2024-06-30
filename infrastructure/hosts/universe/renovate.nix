cc5eac8424ccf69040f081a754a1c3780a934cb7
Sefa Eyeoglu <contact@scrumplex.net>
2024-06-30 15:20:08 -0400

    hosts/universe/renovate: use NixOS module
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- infrastructure/hosts/universe/renovate.nix
+++ infrastructure/hosts/universe/renovate.nix
@@ -1,61 +1,33 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
-  inherit (lib.meta) getExe';
-
-  renovateConfig = pkgs.writeText "renovate.json" (builtins.toJSON {
-    platform = "gitea";
-    endpoint = "https://codeberg.org/api/v1";
-    gitAuthor = "Skippy <renovate@scrumplex.net>";
-    onboardingConfig = {
-      "$schema" = "https://docs.renovatebot.com/renovate-schema.json";
-      extends = ["config:recommended"];
-    };
-    onboardingNoDeps = true;
-
-    repositories = [
-      "Scrumplex/flake"
-      "Scrumplex/duckhub-static"
-      "Scrumplex/honeylinks"
-      "Scrumplex/infrastructure"
-      "Scrumplex/inhibridge"
-      "Scrumplex/skinprox"
-      "Scrumplex/website"
-    ];
-  });
-in {
-  virtualisation.podman.enable = true;
-
+{config, ...}: {
   age.secrets."renovate.env".file = ../../secrets/universe/renovate.env.age;
 
-  systemd.services."renovate" = {
-    description = "Renovate Bot";
-    environment.PODMAN_SYSTEMD_UNIT = "%n";
-    serviceConfig = {
-      Type = "notify";
-      ExecStart = ''
-        ${getExe' pkgs.podman "podman"} run \
-          --name=renovate \
-          --volume=${renovateConfig}:/usr/src/app/config.json \
-          --env=RENOVATE_CONFIG_FILE=/usr/src/app/config.json \
-          --env-file=${config.age.secrets."renovate.env".path} \
-          --rm --replace --detach --sdnotify=conmon --cgroups=no-conmon \
-          ${config.virtualisation.oci-containers.externalImages.images."renovate".ref}
-      '';
-      NotifyAccess = "all";
-      RestartSec = "10s";
-      Restart = "on-abnormal";
+  services.renovate = {
+    enable = true;
+    schedule = "hourly";
+    settings = {
+      platform = "gitea";
+      endpoint = "https://codeberg.org/api/v1";
+      gitAuthor = "Skippy <renovate@scrumplex.net>";
+      onboardingConfig = {
+        "$schema" = "https://docs.renovatebot.com/renovate-schema.json";
+        extends = ["config:recommended"];
+      };
+      onboardingNoDeps = true;
+
+      repositories = [
+        "Scrumplex/flake"
+        "Scrumplex/duckhub-static"
+        "Scrumplex/honeylinks"
+        "Scrumplex/infrastructure"
+        "Scrumplex/inhibridge"
+        "Scrumplex/skinprox"
+        "Scrumplex/website"
+      ];
     };
   };
 
-  systemd.timers."renovate" = {
-    timerConfig = {
-      OnCalendar = "hourly";
-      RandomizedDelaySec = "1m";
-    };
-    wantedBy = ["multi-user.target"];
-  };
+  systemd.services."renovate".serviceConfig.EnvironmentFile = [
+    config.age.secrets."renovate.env".path
+  ];
+  systemd.timers."renovate".timerConfig.RandomizedDelaySec = "5m";
 }
