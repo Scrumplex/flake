058eee6f43bfb82d279f2b233035e8286dcd8e87
Sefa Eyeoglu <contact@scrumplex.net>
2024-08-13 12:50:17 +0200

    nixosConfigurations/dyson/desktop/sway: add Pulsar mouse config
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/dyson/desktop/sway.nix
+++ nixosConfigurations/dyson/desktop/sway.nix
@@ -1,9 +1,19 @@
 {...}: {
   hm.wayland.windowManager.sway.config = {
-    input."2362:628:PIXA3854:00_093A:0274_Touchpad" = {
-      natural_scroll = "enabled";
-      tap = "enabled";
-      tap_button_map = "lrm";
+    input = {
+      "13652:62728:pulsar_X2_V2" = {
+        accel_profile = "adaptive";
+        pointer_accel = "-1.0";
+      };
+      "13652:62727:pulsar_X2_V2" = {
+        accel_profile = "adaptive";
+        pointer_accel = "-1.0";
+      };
+      "2362:628:PIXA3854:00_093A:0274_Touchpad" = {
+        natural_scroll = "enabled";
+        tap = "enabled";
+        tap_button_map = "lrm";
+      };
     };
 
     output = {
