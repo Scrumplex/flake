067015069658bf8eec47d17b26a893c77219399c
Sefa Eyeoglu <contact@scrumplex.net>
2024-09-02 23:23:47 +0200

    nixosConfigurations/common/desktop/gaming: update proton rtsp
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/desktop/gaming.nix
+++ nixosConfigurations/common/desktop/gaming.nix
@@ -26,10 +26,10 @@
     extraCompatPackages = with pkgs; [
       proton-ge-bin
       (proton-ge-bin.overrideAttrs (finalAttrs: _: {
-        version = "GE-Proton9-10-rtsp13";
+        version = "GE-Proton9-11-rtsp15";
         src = pkgs.fetchzip {
           url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-          hash = "sha256-jf1p33Kuqtriycf6gOw/IBdx/ts/P7PJd+pjxonAS/U=";
+          hash = "sha256-3QWJUVkMgZldEXFVsry1FoKVE9y6Tg4IREAruPuL+hk=";
         };
       }))
     ];
