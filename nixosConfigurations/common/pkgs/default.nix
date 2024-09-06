bd2e2e7474db3f966d6587108a9ad441f3e8dda3
Sefa Eyeoglu <contact@scrumplex.net>
2024-09-06 22:56:40 +0200

    nixosConfigurations/common/pkgs: remove unnecessary insecure package
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/pkgs/default.nix
+++ nixosConfigurations/common/pkgs/default.nix
@@ -16,9 +16,6 @@ in {
         "steam-original"
         "steam-run"
       ];
-    config.permittedInsecurePackages = [
-      "jitsi-meet-1.0.8043"
-    ];
 
     overlays = lib.mkAfter [
       (final: prev: {
