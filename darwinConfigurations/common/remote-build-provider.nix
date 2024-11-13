45826bcaaac93e260f9108d8613521fdccc0358c
seth <getchoo@tuta.io>
2024-11-12 20:42:56 -0500

    darwinConfigurations/builder: add getchoo to authorizedKeys

--- darwinConfigurations/common/remote-build-provider.nix
+++ darwinConfigurations/common/remote-build-provider.nix
@@ -18,6 +18,8 @@ in {
     shell = pkgs.bashInteractive;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0NTTsagVihqSLWR9gHcH6cWkADIQdI1YKEuogq71Gw"
+      # getchoo
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpB6mzzsR0vHR/pZ6a60mvMhJS4d0d1TzezbUetXz3i seth@macstadium"
     ];
   };
 
