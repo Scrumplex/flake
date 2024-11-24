6e0a5341437d0b09bc88f1ac25cdc5d1d9a4afa1
Sefa Eyeoglu <contact@scrumplex.net>
2024-11-24 18:57:48 +0100

    pkgs/linux-bpir4: 6.12 -> 6.12.1
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/linux-bpir4.nix
+++ pkgs/linux-bpir4.nix
@@ -4,14 +4,14 @@
   ...
 }:
 linuxManualConfig rec {
-  version = "6.12";
-  modDirVersion = "6.12.0";
+  version = "6.12.1";
+  modDirVersion = "6.12.1";
 
   src = fetchFromGitHub {
     owner = "K900";
     repo = "linux";
-    rev = "b9810763c25c72e1ffc8a676e3436c1576a95059";
-    hash = "sha256-YY4fve8hpJoxciroOjfCgFvVsw4H8HbSXCGrhseqzNY=";
+    rev = "fd7b607fcc673865baf2c29f22735407d6f4f7d7";
+    hash = "sha256-U0KZBZTTsa13+BmdkU0G3OZFSUKWpDKBZBkl15D1pAQ=";
   };
 
   configfile = "${src}/arch/arm64/configs/mt7988a_bpi-r4_defconfig";
