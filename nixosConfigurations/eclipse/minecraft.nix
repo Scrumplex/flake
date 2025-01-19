8bf7187c4e9aa523db3da6008990e506f61331d4
Sefa Eyeoglu <contact@scrumplex.net>
2025-01-19 19:35:16 +0100

    nixosConfigurations/eclipse/minecraft: update atm10 neoforge version
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/eclipse/minecraft.nix
+++ nixosConfigurations/eclipse/minecraft.nix
@@ -61,7 +61,7 @@ in {
         runtimeInputs = [pkgs.jdk21];
 
         text = ''
-          exec java "$@" @libraries/net/neoforged/neoforge/21.1.93/unix_args.txt nogui
+          exec java "$@" @libraries/net/neoforged/neoforge/21.1.95/unix_args.txt nogui
         '';
       };
 
