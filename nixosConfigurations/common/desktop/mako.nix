2285178bbcb23f40abb52be3544e4b31ea323fd8
Sefa Eyeoglu <contact@scrumplex.net>
2025-03-19 21:21:23 +0100

    nixosConfigurations/common/mako: remove critical notification config
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/desktop/mako.nix
+++ nixosConfigurations/common/desktop/mako.nix
@@ -4,20 +4,18 @@
   lib',
   ...
 }: {
-  hm.catppuccin.mako.enable = true;
-  hm.services.mako = {
-    enable = true;
-    font = "Monocraft 10";
-    borderRadius = 12;
-    borderSize = 2;
-    extraConfig = ''
-      [urgency=critical]
-      layer=overlay
-      anchor=top-center
-
-      [mode=dnd]
-      invisible=1
-    '';
+  hm = {
+    catppuccin.mako.enable = true;
+    services.mako = {
+      enable = true;
+      font = "Monocraft 10";
+      borderRadius = 12;
+      borderSize = 2;
+      extraConfig = ''
+        [mode=dnd]
+        invisible=1
+      '';
+    };
+    wayland.windowManager.sway.config.keybindings = lib'.sway.mkExec "${config.hm.wayland.windowManager.sway.config.modifier}+Backspace" "${lib.getExe' config.hm.services.mako.package "makoctl"} dismiss";
   };
-  hm.wayland.windowManager.sway.config.keybindings = lib'.sway.mkExec "${config.hm.wayland.windowManager.sway.config.modifier}+Backspace" "${lib.getExe' config.hm.services.mako.package "makoctl"} dismiss";
 }
