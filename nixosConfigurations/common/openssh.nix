355a1189a9574964a6161a38f52279cb2f665785
Sefa Eyeoglu <contact@scrumplex.net>
2025-04-07 18:42:44 +0200

    nixosConfigurations/openssh: update authorized keys
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/openssh.nix
+++ nixosConfigurations/common/openssh.nix
@@ -7,6 +7,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJV9lYhi0kcwAAjPTMl6sycwCGkjrI0bvTIwpPuXkW2W scrumplex@andromeda"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4jTPHOnfxvBOcVmExcU+j2u9Lsf1OoVG/ols2Met9/ scrumplex@dyson"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJiQEIN+AnXuJFNqw04h/LSGF1bu8cS5PjzgIpn5QTX1 termux@void"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGpFTRRx1FBilZA5epIadTK6GvUDNGdD288F3x6rO+z termux@void"
   ];
 }
