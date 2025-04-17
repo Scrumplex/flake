b198d47754c271ef5909a14000498ba2154fcbb9
Sefa Eyeoglu <contact@scrumplex.net>
2025-04-17 23:12:15 +0200

    openwrt: add honeyjar
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- openwrt/default.nix
+++ openwrt/default.nix
@@ -5,6 +5,7 @@
     packages = {
       openwrt-arecibo = inputs.openwrt-imagebuilder.lib.build (import ./arecibo.nix {inherit profiles;});
       openwrt-brite = inputs.openwrt-imagebuilder.lib.build (import ./brite.nix {inherit profiles;});
+      openwrt-honeyjar = inputs.openwrt-imagebuilder.lib.build (import ./honeyjar.nix {inherit profiles;});
     };
   };
 }
---
+++ openwrt/honeyjar.nix
@@ -0,0 +1,10 @@
+{profiles}:
+profiles.identifyProfile "cudy_m3000-v1"
+// {
+  packages = [
+    "-dnsmasq"
+    "-wpad-basic-mbedtls"
+    "wpad-openssl"
+    "luci"
+  ];
+}
