e484d9cb4c58c3bf95d6583920ab44477349b22c
Sefa Eyeoglu <contact@scrumplex.net>
2025-05-16 19:57:30 +0200

    nixosConfigurations/eclipse/minecraft: add hielke
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/eclipse/minecraft.nix
+++ nixosConfigurations/eclipse/minecraft.nix
@@ -6,6 +6,12 @@
   ...
 }: let
   inherit (builtins) concatStringsSep;
+  inherit (inputs.nix-minecraft.lib) collectFilesAt;
+
+  modpack = pkgs.fetchPackwizModpack {
+    url = "https://github.com/skywardmc/adrenaline/raw/4c72d88842c4a04b30feb0d66a486851d779f417/versions/fabric/1.21.5/pack.toml";
+    packHash = "sha256-oPiO/JhidDeIJVxh4jo1domTo03h2WqX3BQNzCaWXi0=";
+  };
 in {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
   nixpkgs.overlays = [inputs.nix-minecraft.overlays.default];
@@ -15,12 +21,12 @@ in {
     eula = true;
     openFirewall = true;
 
-    servers.alex = {
+    servers.hielke = {
       enable = true;
-      package = pkgs.fabricServers.fabric-1_21_3;
+      package = pkgs.fabricServers.fabric-1_21_5;
 
       jvmOpts = concatStringsSep " " [
-        "-Xmx2G"
+        "-Xmx4G"
         "-XX:+UseG1GC"
         "-XX:+ParallelRefProcEnabled"
         "-XX:MaxGCPauseMillis=200"
@@ -41,63 +47,29 @@ in {
         "-XX:MaxTenuringThreshold=1"
       ];
 
-      #whitelist = {
-      #  "Scrumplex" = "f2873756-429d-413a-b22d-6a976ed0d3f2";
-      #};
+      whitelist = {
+        "Aresiel" = "2295b652-0600-4212-b718-632c1181cb1f";
+        "jopejoe1" = "668e7103-182c-43eb-aa4b-ca8cbc5a5e58";
+        "jstsmthrgk" = "94800a68-96d2-4dcd-a2dd-cff82b5a0e4a";
+        "MjolnirsRevenge" = "1f90dc25-6857-45b7-91fe-b1c905b5331d";
+        "Scrumplex" = "f2873756-429d-413a-b22d-6a976ed0d3f2";
+        "WaterSword" = "fb6301b9-3060-4281-bc17-503f69fb4f53";
+      };
+
+      operators = {
+        "Scrumplex".uuid = "f2873756-429d-413a-b22d-6a976ed0d3f2";
+      };
 
       serverProperties = {
         difficulty = "normal";
+        enable-command-block = true;
         motd = "scrumplex.net";
         spawn-protection = 0;
         white-list = true;
       };
-    };
 
-    servers.atm10 = {
-      enable = false;
-      package = pkgs.writeShellApplication {
-        name = "minecraft-server";
-
-        runtimeInputs = [pkgs.jdk21];
-
-        text = ''
-          exec java "$@" @libraries/net/neoforged/neoforge/21.1.95/unix_args.txt nogui
-        '';
-      };
-
-      jvmOpts = concatStringsSep " " [
-        "-Xms4G"
-        "-Xmx8G"
-        "-XX:+UseG1GC"
-        "-XX:+ParallelRefProcEnabled"
-        "-XX:MaxGCPauseMillis=200"
-        "-XX:+UnlockExperimentalVMOptions"
-        "-XX:+DisableExplicitGC"
-        "-XX:+AlwaysPreTouch"
-        "-XX:G1NewSizePercent=30"
-        "-XX:G1MaxNewSizePercent=40"
-        "-XX:G1HeapRegionSize=8M"
-        "-XX:G1ReservePercent=20"
-        "-XX:G1HeapWastePercent=5"
-        "-XX:G1MixedGCCountTarget=4"
-        "-XX:InitiatingHeapOccupancyPercent=15"
-        "-XX:G1MixedGCLiveThresholdPercent=90"
-        "-XX:G1RSetUpdatingPauseTimePercent=5"
-        "-XX:SurvivorRatio=32"
-        "-XX:+PerfDisableSharedMem"
-        "-XX:MaxTenuringThreshold=1"
-      ];
-
-      serverProperties = {
-        # False positives
-        allow-flight = true;
-        difficulty = "normal";
-        motd = "scrumplex.net";
-        server-port = 25566;
-        spawn-protection = 0;
-        white-list = true;
-        max-tick-time = 180000;
-      };
+      symlinks = collectFilesAt modpack "mods";
+      files = collectFilesAt modpack "config";
     };
   };
 
