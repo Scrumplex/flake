f43959bdd265a120ced8370aec22fe61624d6947
Sefa Eyeoglu <contact@scrumplex.net>
2025-05-17 12:38:56 +0200

    nixosConfigurations/common/mako: fix criteria config
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/desktop/mako.nix
+++ nixosConfigurations/common/desktop/mako.nix
@@ -12,8 +12,8 @@
         font = "Monocraft 10";
         border-radius = "12";
         border-size = "2";
+        "mode=dnd".invisible = 1;
       };
-      criterias."mode=dnd".invisible = "1";
     };
     wayland.windowManager.sway.config.keybindings = lib'.sway.mkExec "${config.hm.wayland.windowManager.sway.config.modifier}+Backspace" "${lib.getExe' config.hm.services.mako.package "makoctl"} dismiss";
   };
