686a3d6a39369737aa3bc3796305b602e2bc51e0
Sefa Eyeoglu <contact@scrumplex.net>
2025-06-06 18:46:10 +0200

    nixosConfigurations/eclipse/minecraft: add cobblemon
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/eclipse/minecraft.nix
+++ nixosConfigurations/eclipse/minecraft.nix
@@ -12,6 +12,11 @@
     url = "https://github.com/skywardmc/adrenaline/raw/4c72d88842c4a04b30feb0d66a486851d779f417/versions/fabric/1.21.5/pack.toml";
     packHash = "sha256-oPiO/JhidDeIJVxh4jo1domTo03h2WqX3BQNzCaWXi0=";
   };
+
+  mrpack2server21 = pkgs.fetchurl {
+    url = "https://github.com/Patbox/mrpack4server/releases/download/0.5.0/mrpack4server-0.5.0.jar";
+    hash = "sha256-6Jvar8Pw9cB+2RLLKca+q2FxZXmWB4DrfFtxKbuqxl8=";
+  };
 in {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
   nixpkgs.overlays = [inputs.nix-minecraft.overlays.default];
@@ -21,8 +26,68 @@ in {
     eula = true;
     openFirewall = true;
 
-    servers.hielke = {
+    servers.cobblemon = {
       enable = true;
+
+      package = pkgs.writeShellApplication {
+        name = "mrpack2server";
+
+        runtimeInputs = [pkgs.jdk21];
+
+        text = ''
+          exec java "$@" -jar ${mrpack2server21} noGui
+        '';
+      };
+
+      jvmOpts = concatStringsSep " " [
+        "-Xmx4G"
+        "-XX:+UseG1GC"
+        "-XX:+ParallelRefProcEnabled"
+        "-XX:MaxGCPauseMillis=200"
+        "-XX:+UnlockExperimentalVMOptions"
+        "-XX:+DisableExplicitGC"
+        "-XX:+AlwaysPreTouch"
+        "-XX:G1NewSizePercent=30"
+        "-XX:G1MaxNewSizePercent=40"
+        "-XX:G1HeapRegionSize=8M"
+        "-XX:G1ReservePercent=20"
+        "-XX:G1HeapWastePercent=5"
+        "-XX:G1MixedGCCountTarget=4"
+        "-XX:InitiatingHeapOccupancyPercent=15"
+        "-XX:G1MixedGCLiveThresholdPercent=90"
+        "-XX:G1RSetUpdatingPauseTimePercent=5"
+        "-XX:SurvivorRatio=32"
+        "-XX:+PerfDisableSharedMem"
+        "-XX:MaxTenuringThreshold=1"
+      ];
+
+      whitelist = {
+        "frecked_Star" = "8c7e6c67-9f59-4f79-afcf-bdff40df6db1";
+        "Honey_Arcus" = "35ab6244-3e9e-452d-a6cd-258868d3cad7";
+        "Scrumplex" = "f2873756-429d-413a-b22d-6a976ed0d3f2";
+      };
+
+      operators = {
+        "Scrumplex".uuid = "f2873756-429d-413a-b22d-6a976ed0d3f2";
+      };
+
+      serverProperties = {
+        difficulty = "normal";
+        enforce-secure-profile = false;
+        motd = "scrumplex.net";
+        spawn-protection = 0;
+        white-list = true;
+      };
+
+      symlinks."local.mrpack" = pkgs.fetchurl {
+        name = "Cobblemon-1.6.1.mrpack";
+        url = "https://cdn.modrinth.com/data/5FFgwNNP/versions/cqaC80tF/Cobblemon%20Official%20%5BFabric%5D%201.6.1.mrpack";
+        hash = "sha256-U22WX5eL8oQb2YJSyOkP4v+ndOJyFiEu345tR8OtOXo=";
+      };
+    };
+
+    servers.hielke = {
+      enable = false;
       package = pkgs.fabricServers.fabric-1_21_5;
 
       jvmOpts = concatStringsSep " " [
