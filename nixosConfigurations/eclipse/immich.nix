05a5ef5287d88f9340e48519e8328eb1d2bf2c96
Sefa Eyeoglu <contact@scrumplex.net>
2025-06-19 09:35:02 +0200

    nixosConfigurations/eclipse/immich: use nixpkgs version
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/eclipse/immich.nix
+++ nixosConfigurations/eclipse/immich.nix
@@ -1,20 +1,8 @@
-{
-  config,
-  pkgs,
-  ...
-}: let
-  nixpkgsWithImmich133 = import (pkgs.fetchFromGitHub {
-    owner = "Scrumplex";
-    repo = "nixpkgs";
-    rev = "50cab7f910f7451480b1631c96df3f9867e754be";
-    hash = "sha256-cX4CtfYjp4VdEm1fwxnJh9t/5bRKzr/03lu7PAMXeBs=";
-  }) {inherit (pkgs) system;};
-in {
+{config, ...}: {
   services.immich = {
     enable = true;
     mediaLocation = "/media/immich-library";
     port = 9121;
-    package = nixpkgsWithImmich133.immich;
   };
 
   users.users.immich.extraGroups = ["video" "render"];
