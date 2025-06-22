55c6aa422fb87db3c40fe3e625f19f11525993c0
Sefa Eyeoglu <contact@scrumplex.net>
2025-06-22 13:54:59 +0200

    nixosConfigurations/common/desktop/wob: fix hex colors
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/common/desktop/wob.nix
+++ nixosConfigurations/common/desktop/wob.nix
@@ -6,9 +6,9 @@
       border_size = 2;
       bar_padding = 8;
       # TODO: get these from somewhere else
-      border_color = "#89dcebff";
-      background_color = "#11111be6";
-      bar_color = "#89dcebff";
+      border_color = "89dcebff";
+      background_color = "11111be6";
+      bar_color = "89dcebff";
     };
   };
 }
