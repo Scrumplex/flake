fbbcf938d7c6daea4b2795650fa4165e0b9dfbb4
Sefa Eyeoglu <contact@scrumplex.net>
2025-08-18 20:07:14 +0200

    openwrt: init xiaomi
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- openwrt/default.nix
+++ openwrt/default.nix
@@ -6,6 +6,7 @@
       openwrt-arecibo = inputs.openwrt-imagebuilder.lib.build (import ./arecibo.nix {inherit profiles;});
       openwrt-brite = inputs.openwrt-imagebuilder.lib.build (import ./brite.nix {inherit profiles;});
       openwrt-honeyjar = inputs.openwrt-imagebuilder.lib.build (import ./honeyjar.nix {inherit profiles;});
+      openwrt-xiaomi = inputs.openwrt-imagebuilder.lib.build (import ./xiaomi.nix {inherit profiles;});
     };
   };
 }
---
+++ openwrt/xiaomi.nix
@@ -0,0 +1,11 @@
+{profiles}:
+profiles.identifyProfile "xiaomi_mi-router-ax3000t-ubootmod"
+// {
+  packages = [
+    "-dnsmasq"
+    "-wpad-basic-mbedtls"
+    "wpad-openssl"
+    "luci"
+    "offloader"
+  ];
+}
