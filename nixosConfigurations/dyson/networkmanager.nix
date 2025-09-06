8af6b3986a6cc1b1d83ff1c0ec7b37b5e11e4b74
Sefa Eyeoglu <contact@scrumplex.net>
2025-09-06 15:00:47 +0200

    nixosConfigurations/dyson/networkmanager: enable iwd
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/dyson/networkmanager.nix
+++ nixosConfigurations/dyson/networkmanager.nix
@@ -6,7 +6,10 @@
     }
   ];
 
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+  };
   services.avahi.enable = true;
 
   hm = {
