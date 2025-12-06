e2cd884affcb085f8be7a5e5f49779f9cbab9943
Sefa Eyeoglu <contact@scrumplex.net>
2025-12-06 01:06:45 +0100

    fix(eclipse/dyndns): disable ipv6 support for now
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/eclipse/dyndns.nix
+++ nixosConfigurations/eclipse/dyndns.nix
@@ -11,7 +11,9 @@
     enable = true;
     zone = "sefa.cloud";
     record = "@";
-    ipv6.enable = true;
+    # Currently our split reverse-proxy setup doesn't allow for this.
+    # Perhaps we should move all internal services into a container so we can assign a completely separate IP address to them?
+    #ipv6.enable = true;
     environmentFile = config.age.secrets."hetzner-api-token.env".path;
   };
 }
