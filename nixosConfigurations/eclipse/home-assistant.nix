280b412ae7eeff4025dc52af1e68316a248c206d
Sefa Eyeoglu <contact@scrumplex.net>
2025-12-10 00:24:52 +0100

    Revert "chore: unset default values"
    This reverts commit d7ca9e0350212e01d61f96409923aff4894d16b9.

--- nixosConfigurations/eclipse/home-assistant.nix
+++ nixosConfigurations/eclipse/home-assistant.nix
@@ -38,10 +38,11 @@
         time_zone = config.time.timeZone;
       };
       http = {
+        server_host = "127.0.0.1";
+        server_port = 8123;
         use_x_forwarded_for = true;
         trusted_proxies = [
           "127.0.0.1"
-          "::1"
         ];
       };
       default_config = {};
