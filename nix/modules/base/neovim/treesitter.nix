5018ca276a389ef3c1c162afaf38492141cac9b3
Sefa Eyeoglu <contact@scrumplex.net>
2025-12-20 13:33:41 +0100

    fix: fix folding
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nix/modules/base/neovim/treesitter.nix
+++ nix/modules/base/neovim/treesitter.nix
@@ -2,7 +2,7 @@
   flake.modules.nixos.base.programs.nixvim = {
     plugins.treesitter = {
       enable = true;
-      folding = false;
+      folding.enable = false;
       settings.indent.enable = true;
     };
   };
