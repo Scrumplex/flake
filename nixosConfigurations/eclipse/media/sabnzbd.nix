e516153b447612b138101d9f595fd763982140c0
Sefa Eyeoglu <contact@scrumplex.net>
2026-01-21 14:31:45 +0100

    feat(sabnzbd): use declarative config
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/eclipse/media/sabnzbd.nix
+++ nixosConfigurations/eclipse/media/sabnzbd.nix
@@ -1,8 +1,36 @@
-{...}: {
+{config, ...}: let
+  fqdn = "nzb.eclipse.sefa.cloud";
+in {
+  age.secrets."sabnzbd-secrets.ini" = {
+    file = ./sabnzbd-secrets.ini.age;
+    owner = "media";
+    group = "media";
+  };
+
   services.sabnzbd = {
     enable = true;
     user = "media";
     group = "media";
+    settings = {
+      misc = {
+        port = 8085;
+        host_whitelist = fqdn;
+        backup_for_duplicates = 1;
+        bandwidth_max = "18M";
+        bandwidth_perc = 100;
+        cache_limit = "1G";
+        complete_dir = "/media/nzb/complete";
+        direct_unpack_tested = 1;
+        download_dir = "/media/nzb/incomplete";
+        history_limit = 250;
+        history_retention = "";
+        history_retention_number = 1;
+        queue_limit = 250;
+      };
+    };
+    secretFiles = [
+      config.age.secrets."sabnzbd-secrets.ini".path
+    ];
   };
 
   systemd.services.sabnzbd.unitConfig.RequiresMountsFor = ["/media"];
@@ -11,8 +39,8 @@
     routers.sabnzbd = {
       entryPoints = ["localsecure"];
       service = "sabnzbd";
-      rule = "Host(`nzb.eclipse.sefa.cloud`)";
+      rule = "Host(`${fqdn}`)";
     };
-    services.sabnzbd.loadBalancer.servers = [{url = "http://localhost:8888";}];
+    services.sabnzbd.loadBalancer.servers = [{url = "http://localhost:${toString config.services.sabnzbd.settings.misc.port}";}];
   };
 }
