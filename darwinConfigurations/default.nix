203eb23ac72c77ccadb47ba59e8703a018da07a6
Sefa Eyeoglu <sefa.eyeoglu@telekom.de>
2026-04-13 09:06:49 +0200

    chore: rename darwin host
    Signed-off-by: Sefa Eyeoglu <sefa.eyeoglu@telekom.de>

--- darwinConfigurations/default.nix
+++ darwinConfigurations/default.nix
@@ -6,14 +6,14 @@
   inherit (inputs) nix-darwin scrumpkgs;
 in {
   flake.darwinConfigurations = {
-    T00179100c = nix-darwin.lib.darwinSystem {
+    LMDT00179100C = nix-darwin.lib.darwinSystem {
       modules = [
         {
           system.configurationRevision = self.rev or self.dirtyRev or null;
           nixpkgs.hostPlatform = "aarch64-darwin";
         }
 
-        ./T00179100c
+        ./LMDT00179100C
       ];
 
       specialArgs = {
