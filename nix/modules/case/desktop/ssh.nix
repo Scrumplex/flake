a79fc957aad2dabd8e5d7d9583b0e2cdd4c68f73
Sefa Eyeoglu <contact@scrumplex.net>
2026-05-24 09:12:10 +0200

    refactor(ssh): use settings option
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nix/modules/case/desktop/ssh.nix
+++ nix/modules/case/desktop/ssh.nix
@@ -1,37 +1,26 @@
-{lib, ...}: let
-  mkGitAlias = host: {
-    "${host}".user = "git";
-  };
-in {
+{
   flake.modules.homeManager.desktop.programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks = lib.mkMerge [
-      (mkGitAlias "gitlab.com")
-      (mkGitAlias "git.sr.ht")
-      (mkGitAlias "github.com")
-      (mkGitAlias "codeberg.org")
-      (mkGitAlias "gitlab.freedesktop.org")
-      {
-        "*" = {
-          controlMaster = "auto";
-          controlPath = "~/.ssh/sockets/master-%r@%n:%p";
-          controlPersist = "10m";
-        };
+    settings = {
+      "*" = {
+        controlMaster = "auto";
+        controlPath = "~/.ssh/sockets/master-%r@%n:%p";
+        controlPersist = "10m";
+      };
 
-        "galileo.sefa.cloud" = {
-          user = "root";
-        };
+      "galileo.sefa.cloud" = {
+        user = "root";
+      };
 
-        "eclipse.sefa.cloud" = {
-          user = "root";
-        };
+      "eclipse.sefa.cloud" = {
+        user = "root";
+      };
 
-        "scrumplex.net" = {
-          user = "root";
-        };
-      }
-    ];
+      "scrumplex.net" = {
+        user = "root";
+      };
+    };
   };
 }
