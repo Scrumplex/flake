18d2c94192a4e72ee271f84eb5420b0beebda763
Sefa Eyeoglu <contact@scrumplex.net>
2026-06-11 11:02:35 +0200

    fix: use honeylinks
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- nixosConfigurations/universe/honeylinks-website.nix
+++ nixosConfigurations/universe/honeylinks-website.nix
@@ -5,11 +5,12 @@
   pkgs,
   ...
 }: {
+  nixpkgs.overlays = [inputs.honeylinks-website.overlays.default];
+
   services.nginx.virtualHosts."honeyarcus.art" = lib.mkMerge [
     config.common.nginx.vHost
     {
-      # TODO: use overlay once Nixpkgs Node.js tooling works again
-      root = inputs.honeylinks-website.packages.${pkgs.stdenv.hostPlatform.system}.honeylinks-website;
+      root = pkgs.honeylinks-website;
       locations = {
         "~* \.html$".extraConfig = ''
           expires 1h;
