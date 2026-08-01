91efb6001aefceeb1f1c4b3964213656dde35a0a
Sefa Eyeoglu <contact@scrumplex.net>
2026-08-01 13:41:15 +0200

    chore: drop unused packages
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,30 +1,14 @@
 {...}: {
-  perSystem = {
-    config,
-    pkgs,
-    ...
-  }: {
+  perSystem = {pkgs, ...}: {
     packages = {
       channel-notifier = pkgs.python3.pkgs.callPackage ./channel-notifier {};
 
       ha-solarman = pkgs.home-assistant.python.pkgs.callPackage ./ha-solarman.nix {};
-
-      linux-bpir4 = pkgs.callPackage ./linux-bpir4.nix {};
-      uboot-bpir4 = pkgs.callPackage ./uboot-bpir4.nix {};
-      banana-pi-r4-firmware = pkgs.callPackage ./banana-pi-r4-firmware.nix {
-        uboot = config.packages.uboot-bpir4;
-      };
     };
   };
   flake.overlays.legacy = final: _: {
     channel-notifier = final.python3.pkgs.callPackage ./channel-notifier {};
 
     ha-solarman = final.home-assistant.python3Packages.callPackage ./ha-solarman.nix {};
-
-    linux-bpir4 = final.callPackage ./linux-bpir4.nix {};
-    uboot-bpir4 = final.callPackage ./uboot-bpir4.nix {};
-    banana-pi-r4-firmware = final.callPackage ./banana-pi-r4-firmware.nix {
-      uboot = final.uboot-bpir4;
-    };
   };
 }
